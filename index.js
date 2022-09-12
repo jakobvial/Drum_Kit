9cd75de683ba9725b94872d25862a52947319be0
Jakob Viktor Albrechtsen <jakobvial@gmail.com>
2022-09-12 19:45:28 +0200

    Add sound and background images

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let buttons = document.querySelectorAll(".drum");
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
-        alert("I got clicked!");
+        let audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
     });
 }
